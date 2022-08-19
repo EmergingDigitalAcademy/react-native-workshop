448e7135dc4546b1728726115cb1a4b3f9494769
masonleonhart <mason.leonhart@gmail.com>
2022-08-19 15:02:31 -0500

    added safe area view and dependencies, created eda themed app icon and splash image

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { Provider as PaperProvider } from "react-native-paper";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+import { Text } from "react-native";
+
+// import TopStack from "./src/components/navigators/TopStack";
+
+const TopStack = () => <SafeAreaView style={{flex: 1, justifyContent: "flex-end"}}><Text>Hello There</Text></SafeAreaView>
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <PaperProvider>
+          <StatusBar barStyle="dark" animated={true} />
+          <TopStack />
+        </PaperProvider>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
